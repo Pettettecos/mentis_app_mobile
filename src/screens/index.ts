fcdeffc82fb81ea82837da43d5b25b42137a220f
Pettecco <petterson.ikaro@gec.inatel.br>
2026-05-07 20:34:43 -0300

    refactor: remove legacy app entry points and auth screens

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,1 +1,3 @@
-export * from './Auth';
+export { LoginScreen } from './LoginScreen';
+export { ForgotPasswordScreen } from './ForgotPasswordScreen';
+export { ResetPasswordScreen } from './ResetPasswordScreen';
