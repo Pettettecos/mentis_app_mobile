d81bfbed07f21b169b1ca2a24b5f642220fc6280
Pettecco <petterson.ikaro@gec.inatel.br>
2026-05-23 17:56:35 -0300

    feat: add EmployeeDashboardScreen export and update translations

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,3 +1,4 @@
 export { LoginScreen } from './LoginScreen';
 export { ForgotPasswordScreen } from './ForgotPasswordScreen';
 export { ResetPasswordScreen } from './ResetPasswordScreen';
+export { EmployeeDashboardScreen } from './EmployeeScreens/EmployeeDashboardScreen';
